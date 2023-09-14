879603be9343a3f92e27477f9f84341f4dc7999a
jatin verma <jv.jatinverma.jv@gmail.com>
2023-09-14 15:38:19 +0530

    working on backend

---
+++ index.js
@@ -0,0 +1,38 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import cookieParser from "cookie-parser";
+
+import userRoutes from "./routes/users.js";
+import authRoutes from "./routes/auths.js";
+import tweetRoutes from "./routes/tweets.js";
+
+const app = express();
+
+// so that we can read data from env file
+dotenv.config();
+
+// connecting to mongodb
+const connect = () => {
+  // to get rid of warning strictQuery from console
+  mongoose.set("strictQuery", false);
+  mongoose
+    .connect(process.env.MONGO)
+    .then(() => {
+      console.log("Connected to mongodb database");
+    })
+    .catch((err) => {
+      throw err;
+    });
+};
+
+app.use(cookieParser());
+app.use(express.json());
+app.use("/api/users", userRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/tweets", tweetRoutes);
+
+app.listen(8000, () => {
+  connect();
+  console.log("Listening to port 8000");
+});
---
+++ models/User.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+// creating user schema
+const UserSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    profileProfile: { type: String },
+    followers: { type: Array, defaultValue: [] },
+    following: { type: Array, defaultValue: [] },
+    description: { type: String },
+    profilePicture: { type: String },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("User", UserSchema);
